5416098b583518f835cc8a56ab3633a43f38c48d
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-11-21 17:50:47 +0000

    week 13 synthesizing

---
+++ picklecache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Work with pickle file """
+
+
+import os
+import pickle
+
+
+class PickleCache(object):
+    """PickleCache class"""
+    def __init__(self, file_path='datastore.pkl', autosync=False):
+        """Class contructor"""
+        self.__file_path = file_path
+        self.__data = {}
+        self.autosync = autosync
+        self.load()
+
+    def __setitem__(self, key, value):
+        """This function creates or updates dictionary dataset."""
+        self.__data[key] = value
+
+    def __len__(self):
+        """This function measures length of data."""
+        sdata_length = len(self.__data)
+        return sdata_length
+
+    def __getitem__(self, key):
+        """This function tests whether key exist in file dictionary."""
+        if self.autosync is True:
+            self.flush()
+        try:
+            self.__data[key]
+        except (TypeError, KeyError) as errors:
+            raise errors
+
+    def __delitem__(self, key):
+        """This function deletes dictionary by key."""
+        if self.autosync is True:
+            self.flush()
+        del self.__data[key]
+
+    def load(self):
+        """This function reads and tests pickle file, and loads it into
+        file path"""
+        if os.path.exists(self.__file_path) and\
+           os.path.getsize(self.__file_path) > 0:
+            read_file = open(self.__file_path, 'r')
+            self.__data = pickle.load(read_file)
+            read_file.close()
+
+    def flush(self):
+        """This function writes data in pickle file"""
+        write_file = open(self.__file_path, 'w')
+        pickle.dump(self.__data, write_file)
+        write_file.close()
